63187a580071b05d1e1100c7a1dd47f27cb900fe
Lucas Costa <lucasfrct@users.noreply.github.com>
2020-01-02 11:55:13 -0300

    Create Home Component

---
+++ project/app/home/home.component.js
@@ -0,0 +1,18 @@
+(()=> {
+    "use strict";
+    angular
+        .module("ebd")
+        .config(HomeConfig)
+        .component("home", {
+            templateUrl: "app/home/home.html",
+            controller: [ "$scope", HomeController ],
+        });
+
+    function HomeConfig( $routeProvider ) {
+        $routeProvider.when("/", { template: "<home></home>" });
+    };
+
+    function HomeController ( $scope ) {
+        $scope.home = "Load Home!";
+    }
+})();
